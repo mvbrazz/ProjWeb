c46e8b26d1b5def6236d51e7b361baefccb0ef07
Marcus Vinicus <44820490+mvbrazz@users.noreply.github.com>
2021-07-29 19:04:59 -0300

    Implementação de busca por nomes e validação do login

--- netflix javascript.js
+++ netflix javascript.js
@@ -19,22 +19,27 @@ function Login(conteudo_login,menu2,menu3,erro_login) {
    
    var nome = document.querySelector("#nome");
    var senha = document.querySelector("#senha");
+   var tamanhoLogin = nome.value;
+   var tamanhoSenha = senha.value;
    
-
-   axios.post('https://reqres.in/api/login',{
-      email: nome.value,
-      password: senha.value
-    })
-    .then(function (response) {
-      console.log(response.data.token);
-      localStorage.setItem('chave',`${response.data.token}`);
-      logado(menu2,menu3);
-    })
-    .catch(function (error) {
-      console.log(error);
-      document.getElementById(erro_login).style.display = 'block';
-    });
-   
+   if(tamanhoLogin.length < 4 || tamanhoSenha.length < 4){
+      alert("Você precisa digitar o login e a senha (4 letras). ");
+   }
+   else{
+      axios.post('https://reqres.in/api/login',{
+         email: nome.value,
+         password: senha.value
+      })
+      .then(function (response) {
+         console.log(response.data.token);
+         localStorage.setItem('chave',`${response.data.token}`);
+         logado(menu2,menu3);
+      })
+      .catch(function (error) {
+         console.log(error);
+         document.getElementById(erro_login).style.display = 'block';
+      });
+   }
 }
 
 function logado(menu2,menu3) {
@@ -49,24 +54,48 @@ function logado(menu2,menu3) {
 
 }
 
-function personagemRetornado(id){
-   return axios.get(`https://rickandmortyapi.com/api/character/${id.value}`)
+function personagemRetornado(nomeP){
+   return axios.get(`https://rickandmortyapi.com/api/character/?name=${nomeP.value}`)
 }
 
 function Pesquisa(menu3,imagem_personagem) {
-   var id = document.querySelector("#busca_personagem");
-   var im= document.querySelector("#imagem_personagem");
-   var dados;
+   //var id = document.querySelector("#busca_personagem");
+   //var menuImagem = document.querySelector('#imagem_personagem');
 
-   dados = personagemRetornado(id);
+   var nomePersonagem = document.querySelector("#busca_personagem");
+   let menuImagem = document.querySelector('#imagem_personagem');
+   var vetPersonagens;
+   vetPersonagens = personagemRetornado(nomePersonagem);
 
-   dados.then(function(resposta){
-      console.log(resposta.data.image);
-      document.getElementById(imagem_personagem).style.backgroundImage = `url("${resposta.data.image}")`;
-           
-   }) 
+   varVetImagem = [];
+  
+   while(menuImagem.firstChild){
+      menuImagem.removeChild(menuImagem.firstChild);
+   }
 
-   document.getElementById(imagem_personagem).style.display = 'block';
-     
-   
+   vetPersonagens.then(function(resposta){
+      //console.log(resposta.data.results.length);
+      var ajuda = [];
+        
+      for(i = 0;i<resposta.data.results.length;i++){
+         let imagemGeral = document.createElement('img');
+         varVetImagem[i] = imagemGeral;
+      }
+      
+      for(i = 0;i<resposta.data.results.length;i++){
+         //document.getElementById(imagem_personagem).style.backgroundImage = `url("${resposta.data.image}")`;
+         //console.log(resposta.data.results[i].image);
+         
+         
+         ajuda[i] = resposta.data.results[i].image;
+         console.log(ajuda[i]);
+    
+         varVetImagem[i].src = ajuda[i];
+         varVetImagem[i].style.width = "120px";
+         varVetImagem[i].style.height = "120px";
+         menuImagem.appendChild(varVetImagem[i]);
+      } 
+         
+   })
+
 }
